5f82ddca0902d0927f68ea4947c581b16799e3fc
ruriknj <ruriknj@gmail.com>
2020-09-29 15:23:54 -0300

    implantado DTO

--- backend/src/main/java/com/dev/superior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/dev/superior/dscatalog/resources/CategoryResource.java
@@ -1,6 +1,5 @@
 package com.dev.superior.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.dev.superior.dscatalog.entities.Category;
+import com.dev.superior.dscatalog.dto.CategoryDTO;
 import com.dev.superior.dscatalog.services.CategoryService;
 
 // IMPLEMENTA O CONTROLADOR REST -> Resources
@@ -22,9 +20,9 @@ public class CategoryResource {
 	private CategoryService service;
 
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
+	public ResponseEntity<List<CategoryDTO>> findAll() {
 		
-		List<Category> list = service.findAll();
+		List<CategoryDTO> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 		
--- backend/src/main/java/com/dev/superior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/dev/superior/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.dev.superior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dev.superior.dscatalog.dto.CategoryDTO;
 import com.dev.superior.dscatalog.entities.Category;
 import com.dev.superior.dscatalog.repositories.CategoryRepository;
 
@@ -16,9 +18,11 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true) // usa-se para o metodo de somente leitura ->( readOnly = true)
-	public List<Category> findAll() {
+	public List<CategoryDTO> findAll() {
 		
-		return repository.findAll();
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(X -> new CategoryDTO(X)).collect(Collectors.toList()); // função lambda
 		
 	}
 	
