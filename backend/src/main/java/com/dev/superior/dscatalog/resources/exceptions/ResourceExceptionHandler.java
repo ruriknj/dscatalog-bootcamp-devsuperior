f9c9f8fcddbf67172dfee32996d7cc7bfde02ade
ruriknj <ruriknj@gmail.com>
2020-10-01 10:56:40 -0300

    Delete Category and improvement exceptions

--- backend/src/main/java/com/dev/superior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/dev/superior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.dev.superior.dscatalog.services.exceptions.DatabaseException;
 import com.dev.superior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice //intercepta as exceções na camada de Resource (REST)
@@ -16,16 +17,29 @@ public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
-		
+		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
-		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setStatus(status.value());
 		err.setError("Resource not found");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
 		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		return ResponseEntity.status(status).body(err);
 		
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
 	}
 	
 }
--- backend/src/main/java/com/dev/superior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/dev/superior/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dev.superior.dscatalog.dto.CategoryDTO;
 import com.dev.superior.dscatalog.entities.Category;
 import com.dev.superior.dscatalog.repositories.CategoryRepository;
+import com.dev.superior.dscatalog.services.exceptions.DatabaseException;
 import com.dev.superior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service  // registrar a class como um componente de injeção de independencia (gerenciar as injeções de dependências)
@@ -58,6 +61,17 @@ public class CategoryService {
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
 			
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id );
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integraty viaolation");
 		}
 	}
 	
