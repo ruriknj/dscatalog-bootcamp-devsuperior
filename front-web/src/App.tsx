ae4def9abcbb357bf8f7c95a54a079e7e6ed54e3
ruriknj <ruriknj@gmail.com>
2020-10-05 15:20:23 -0300

    Uso do useState, useEffect e ciclo de vida

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,35 @@
-import React from "react";
-import Alert from "./Alert";
+import React, {useState, useEffect} from "react";
+
+// useState -> ver documentação React do Hooks
 
 const App = () => {
+    const [counter, setCounter] = useState(0);
+
+    useEffect( () => {
+        console.log('O contador mudou de valor')   // fazer a chamada para API
+    }, [counter]);
+
+     
+
   return (
     <div className="container mt-5">
-      <Alert text = "Rurik" />
-      <Alert text = "BootCamp" />
-      <Alert />
+      <button 
+      className="btn btn-primary mr-5"
+      onClick={ () => setCounter(counter + 1)}
+      >
+        +
+      </button>
+      <span>
+        {counter}     {/* binding  ligação do js com o html */}
+      </span>
+      <button 
+      className="btn btn-primary ml-5"
+      onClick={ () => setCounter(counter - 1) }
+      >
+        -
+      </button>
+        {counter > 5 &&  <h1>O valor é maior que 5</h1>}
+        {counter <= 5 &&  <h1>O valor é menor ou igual a 5</h1>}
     </div>
   );
 }
