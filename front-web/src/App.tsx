ac0805faeb2a79cf184785dd907bb421440a74bc
ruriknj <ruriknj@gmail.com>
2020-10-06 15:32:42 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import './core/assets/styles/custom.scss'
 import './app.scss'
+import Navbar from "./core/components/Navbar";
 
 // useState -> ver documentação React do Hooks
 
 const App = () => {
-
   return (
-  <div>
- <h1>App</h1>
-  </div>
+ <Navbar />
   );
-
 }
 export default App;
