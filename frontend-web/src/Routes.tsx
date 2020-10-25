c1e354cd9a024f32f31ae38560f59f60ee5edc88
ruriknj <ruriknj@hotmail.com>
2020-10-24 22:37:58 -0300

    Finalizando a estrutura base das rotas do Admin

--- frontend-web/src/Routes.tsx
+++ frontend-web/src/Routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Navbar from './core/components/Navbar';
 import Admin from './pages/Admin';
 import Catalog from './pages/Catalog';
@@ -9,20 +9,21 @@ import Home from './pages/Home';
 const Routes = () => (
     <BrowserRouter>
         <Navbar />
-    <Switch>
-        <Route path="/" exact>
-            <Home />
-        </Route>
-        <Route path="/products" exact>
-            <Catalog />
-        </Route>
-        <Route path="/products/:productId">
-            <ProductDetails />
-        </Route>
-        <Route path="/admin">
-            <Admin />
-        </Route>
-    </Switch>
+        <Switch>
+            <Route path="/" exact>
+                <Home />
+            </Route>
+            <Route path="/products" exact>
+                <Catalog />
+            </Route>
+            <Route path="/products/:productId">
+                <ProductDetails />
+            </Route>
+            <Route path="/admin">
+                <Redirect to="/admin/products" />
+                <Admin />
+            </Route>
+        </Switch>
     </BrowserRouter>
 
 );
